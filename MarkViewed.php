5f676206454b0b8ee5bf53a768e20ca944d9610e
Siyun Wang <30507734+keiraaaaa@users.noreply.github.com>
2018-05-09 18:44:04 -0400

    Add files via upload

--- MarkViewed.php
+++ MarkViewed.php
@@ -5,18 +5,14 @@
 
 	$conn = connectDB();
 
- 	$query1 = "select * from Company natural join JobInfo natural join JobNotifications where sid='$sid' and ViewStstus='New' and jid not in (select jid from JobApply where sid = '$sid')";
-    $result1 = mysqli_query($conn, $query1) or die('Query failed: ' . mysqli_error($conn));
-
- // 	$query = "insert into JobApply (`sid`,`jid`,`cid`) VALUES ('".$sid."','".$jid."','".$row1["cid"]."')";
-	// $result = mysqli_query($conn, $query) or die('Query failed: ' . mysqli_error($conn));
+ 	$query = "update JobNotifications natural join (select * from Company natural join JobInfo natural join JobNotifications where sid='$sid' and ViewStstus='New' and jid not in (select jid from JobApply where sid = '$sid') )as a set JobNotifications.ViewStstus='Viewed'";
+	$result = mysqli_query($conn, $query) or die('Query failed: ' . mysqli_error($conn));
 	
-	// if ($result === TRUE) {
-	// 	$_SESSION['sid'] = $sid;
-	// 	$_SESSION['jid'] = $jid;
-	// 	header("Location: MarkViewed.php");
-	// }
-	// else {
-	// 	echo "Error updating record: " . $conn->error;
-	// }
+	if ($result === TRUE) {
+		$_SESSION['sid'] = $sid;
+		header("Location: mainPage.php");
+	}
+	else {
+		echo "Error updating record: " . $conn->error;
+	}
 ?>
